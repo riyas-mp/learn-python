5d5ed6e5da0e47cc71e364ee9b3ef3fae42bb2ab
salih-kv <profileofsalih@gmail.com>
2025-04-28 21:16:58 +0530

    Add check even or odd script

---
+++ exercises/2_check_even_odd.py
@@ -0,0 +1,9 @@
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
+
+
+# Here, `%` is the modulus operator, which gives the remainder of the division of `number` by `2`. 
+# If the remainder is `0`, the number is even; otherwise, it is odd.    
